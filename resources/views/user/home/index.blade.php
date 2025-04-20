ffd228e0f7bc85a554bcd2d5701e981112681bd4
zihadurrahmandev <zihadurrahman.dev@gmail.com>
2025-04-20 19:17:39 +0600

    Create index.blade.php

---
+++ resources/views/user/home/index.blade.php
@@ -0,0 +1,9 @@
+<div class="col-xl-12 col-md-12 col-sm-12">
+    <div class="card height-equal">
+        <div class="card-header">
+            <h3>For Account Activation</h3>
+        </div>
+        <div class="card-body text-center">
+            <a href="{{ route('user.activate.account') }}" class="btn btn-danger btn-lg">Activate Account</a>
+        </div>
+    </div>
