ae5255b5c6b47bb5c704a284d9357e5bc14f0767
zihadurrahmandev <zihadurrahman.dev@gmail.com>
2025-04-20 16:22:13 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,15 @@
     Route::get('/user/gmail', 'gmailSell')->name('user.gmail');
     Route::post('/user/gmail', 'gmailSellMake');
+
+
+
+
+//ADMIN ROUTES
+Route::prefix('admin/gmail-sells')->name('admin.gmail-sells.')->group(function () {
+    Route::get('/', [\App\Http\Controllers\AdminGmailController::class, 'index'])->name('index');
+    Route::post('/approve/{id}', [\App\Http\Controllers\AdminGmailController::class, 'approve'])->name('approve');
+    Route::patch('/reject/{id}', [\App\Http\Controllers\AdminGmailController::class, 'reject'])->name('reject');
+    Route::delete('/delete/{id}', [\App\Http\Controllers\AdminGmailController::class, 'destroy'])->name('delete');
+    Route::post('/update', [\App\Http\Controllers\AdminGmailController::class, 'updateSettings'])->name('update');
+
+});
