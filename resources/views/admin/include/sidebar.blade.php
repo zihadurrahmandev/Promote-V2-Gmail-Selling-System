7df82156251a76ef55bfbd36593284ba371b76c5
zihadurrahmandev <zihadurrahman.dev@gmail.com>
2025-04-20 16:24:10 +0600

    Create sidebar.blade.php

---
+++ resources/views/admin/include/sidebar.blade.php
@@ -0,0 +1,12 @@
+  <li class="sidebar-list">
+    <i class="fa fa-envelope"></i>
+    <a class="sidebar-link sidebar-title" href="{{ route('admin.gmail-sells.index') }}">
+        <svg class="stroke-icon">
+            <use href="{{ asset('/') }}admin/assets/svg/icon-sprite.svg#stroke-email"></use>
+        </svg>
+        <svg class="fill-icon">
+            <use href="{{ asset('/') }}admin/assets/svg/icon-sprite.svg#fill-email"></use>
+        </svg>
+        <span class="">Sell Gmail</span>
+    </a>
+</li>
